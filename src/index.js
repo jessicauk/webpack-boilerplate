54df904904c2e0267a808e5713a98dc284c5d2ee
Jessica Apolinar <jessica_220691@hotmail.com>
2021-02-21 02:28:54 -0600

    feat: Add basic ci configuration. (#1)
    * feat: Add basic ci configuration.
    
    * mend
    
    feat: Remove package-json file from gitignore.
    
    * mend
    
    feat: Remove package-json file from gitignore.
    
    * mend
    
    feat: Add test with moca and chai.
    
    Co-authored-by: Jessica Apolinar <jessica.uk220691@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ console.log("HELLO WORLD");
 document.getElementById("button-test").addEventListener('click',function () {
     console.log("YOU ARE CLICKING ON THE BUTTON!")
 })
+
+module.exports = true;
